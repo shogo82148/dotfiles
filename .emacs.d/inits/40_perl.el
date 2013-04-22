b912bc45b49750b70a9a550e706c3331c8fa0db9
Ichinose Shogo <shogo82148@gmail.com>
2013-04-23 08:02:00 +0900

    Perlのインデント設定を変更

--- .emacs.d/inits/40_perl.el
+++ .emacs.d/inits/40_perl.el
@@ -2,6 +2,14 @@
 (setq auto-mode-alist (cons '("\\.t$" . cperl-mode) auto-mode-alist))
 (add-hook 'cperl-mode-hook
           '(lambda ()
-                  (cperl-set-style "PerlStyle"))) (cperl-set-style "PerlStyle")
+             (cperl-set-style "PerlStyle")
+             (setq-default indent-tabs-mode nil) ; インデントにタブを使用しない
+             (setq cperl-indent-level 4
+                   cperl-close-paren-offset -4
+                   cperl-continued-statement-offset 4
+                   cperl-indent-parens-as-block t
+                   cperl-tab-always-indent t)
+))
+
 
 
