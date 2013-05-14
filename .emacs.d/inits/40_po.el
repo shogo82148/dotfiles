54ab3d4c80206404d020262f58a7d6f0f0d3f296
Ichinose Shogo <shogo82148@gmail.com>
2013-05-14 10:07:23 +0900

    po-modeを追加

---
+++ .emacs.d/inits/40_po.el
@@ -0,0 +1,4 @@
+(autoload 'po-mode "po-mode"
+             "Major mode for translators to edit PO files" t)
+   (setq auto-mode-alist (cons '("\\.po\\'\\|\\.po\\." . po-mode)
+                                auto-mode-alist))
