35a2b96229ebe2be1698d91fb6618b46575b2a5f
Ichinose Shogo <shogo82148@gmail.com>
2013-08-22 22:06:38 +0900

    シンボリックリンク張るスクリプト追加

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cd $(dirname $0)
+for dotfile in .?*
+do
+    if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != 'vimfiles' ] && [ $dotfile != '.gitmodules' ] && [ $dotfile != '.gitignore' ]
+    then
+        ln -Fis "$PWD/$dotfile" $HOME
+    fi
+done
+
+if [ ! -d ~/.vim ]
+then
+    ln -s ~/.dotfiles/vimfiles ~/.vim
+fi
