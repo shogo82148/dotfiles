b0ef3036318978a667919e78492b05c070646162
Ichinose Shogo <shogo82148@gmail.com>
2014-06-28 11:13:02 +0900

    coffee-modeをインストール

---
+++ .emacs.d/inits/40_coffee.el
@@ -0,0 +1,13 @@
+;; coffee script
+
+(autoload 'coffee-mode "coffee-mode" "Major mode for editing CoffeeScript." t)
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+;; automatically clean up bad whitespace
+(setq whitespace-action '(auto-cleanup))
+;; only show bad whitespace
+(setq whitespace-style '(trailing space-before-tab indentation empty space-after-tab))
+
+;; This gives you a tab of 2 spaces
+(custom-set-variables '(coffee-tab-width 2))
