a3b3a16e45b96bffffc4798c3eef8e803d629cb7
Ichinose Shogo <shogo82148@gmail.com>
2014-06-28 10:15:22 +0900

    VS等のデフォルトの設定に合わせて、インデント等の設定を修正

--- .emacs.d/inits/40_csharp.el
+++ .emacs.d/inits/40_csharp.el
@@ -3,3 +3,14 @@
 (autoload 'csharp-mode "csharp-mode.el"
   "Major mode for editing C# files" t)
 (add-to-list 'auto-mode-alist '("\\.cs$" . csharp-mode))
+
+;; インデントはタブのみ、4文字幅
+(add-hook 'csharp-mode-hook
+          (lambda ()
+            (setq default-tab-width 4)
+            (setq indent-tabs-mode t)
+	    (c-set-offset 'substatement-open 0)
+	    (c-set-offset 'case-label '+)
+	    (c-set-offset 'arglist-intro '+)
+	    (c-set-offset 'arglist-close 0)
+            ))
