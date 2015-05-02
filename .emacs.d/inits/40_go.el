f7d6b7a533458320d39ed4d56f65dc245e3a618c
Ichinose Shogo <shogo82148@gmail.com>
2015-05-02 22:46:07 +0900

    go-modeを更新, goimportsを有効化

--- .emacs.d/inits/40_go.el
+++ .emacs.d/inits/40_go.el
@@ -4,9 +4,9 @@
 (autoload 'go-mode "go-mode.el"
   "Major mode for editing golang files" t)
 (add-to-list 'auto-mode-alist '("\\.go$" . go-mode))
-
 (add-hook 'go-mode-hook
       '(lambda ()
          (setq tab-width 2) ; タブ幅を2にする
+	 (setq gofmt-command "goimports")
          (add-hook 'before-save-hook 'gofmt-before-save) ; 保存時に go fmt する
          ))
