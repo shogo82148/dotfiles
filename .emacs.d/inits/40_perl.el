226c9a9b3cdad803afd80829ee079de28acf84e7
Ichinose Shogo <shogo82148@gmail.com>
2015-05-12 11:56:17 +0900

    Perlのpackage名とファイル名があっているかテスト

--- .emacs.d/inits/40_perl.el
+++ .emacs.d/inits/40_perl.el
@@ -11,5 +11,28 @@
                    cperl-tab-always-indent t)
 ))
 
+;;; package名とファイル名が一致していなかった場合に警告
+;;; http://d.hatena.ne.jp/syohex/20121214/1355464141
+(defun cperl-get-package-name ()
+  (save-excursion
+    (goto-char (point-min))
+    (if (re-search-forward "^\\s-*package\\s-+\\([^ ;]+\\)" nil t)
+        (match-string-no-properties 1)
+      "")))
 
+(defun cperl-check-package-name ()
+  (interactive)
+  (let* ((path (file-name-sans-extension (buffer-file-name)))
+         (package (replace-regexp-in-string "::" "/" (cperl-get-package-name)))
+         (package-path (substring path (- (length path) (length package)))))
+    (cond ((string= package "")
+           (error "ぱっけーじせんげんがみつかりません＞＜"))
+          ((not (string= package-path package))
+           (error (concat "ぱっけーじめいと、ほぞんされているぱすが、ちがうきがします！\n"
+                          "ちゃんとなおしてください＞＜"))))))
 
+(defun cperl-check-package-name-hook ()
+  (if (eq major-mode 'cperl-mode)
+      (cperl-check-package-name)))
+
+(add-hook 'after-save-hook 'cperl-check-package-name-hook)
