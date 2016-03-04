88651a29d39e016e0380fedc6291babae70d40ae
Ichinose Shogo <shogo.ichinose@fuller.co.jp>
2016-03-04 14:04:21 +0900

    change js3-mode into js2-mode, and update js2-mode

--- .emacs.d/inits/40_javascript.el
+++ .emacs.d/inits/40_javascript.el
@@ -1,11 +1,3 @@
 ;Javascript向け設定
-(require 'js3-mode)
-(add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
-(add-hook 'js3-mode-hook
-          (lambda ()
-            (setq js3-indent-level 4)
-            (setq js3-mode-dev-mode-p t)
-            (setq js3-auto-indent-p t)
-            (setq js3-enter-indents-newline t)
-            (setq js3-indent-on-enter-key t)
-            ))
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
