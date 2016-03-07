9693462ecba5e76fe76cf538a6f6e7863da2d022
Ichinose Shogo <shogo.ichinose@fuller.co.jp>
2016-03-07 13:22:37 +0900

    add js2-global-externs

--- .emacs.d/inits/40_javascript.el
+++ .emacs.d/inits/40_javascript.el
@@ -1,3 +1,7 @@
 ;Javascript向け設定
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+; 外部で定義されているはずの変数・関数の警告抑制
+(setq js2-global-externs
+      (list "require" "describe" "it" "expect"))
