56ca8997da6e5be9cbd0ea0a8299197d29e6f7b1
Ichinose Shogo <shogo.ichinose@fuller.co.jp>
2016-03-07 13:24:28 +0900

    autoload nginx-mode

--- .emacs.d/inits/40_nginx.el
+++ .emacs.d/inits/40_nginx.el
@@ -1,5 +1,5 @@
-(require 'nginx-mode)
+(autoload 'nginx-mode "nginx-mode" nil t)
 (add-hook 'conf-mode-hook
 	  (lambda ()
 	    (when (string-match "nginx" (buffer-file-name))
-	                    (nginx-mode))))
+	      (nginx-mode))))
