35b3893be1348c6795800b0dd078f741e3258448
Ichinose Shogo <shogo.ichinose@fuller.co.jp>
2016-03-07 13:22:07 +0900

    add web-mode

---
+++ .emacs.d/inits/40_html.el
@@ -0,0 +1,26 @@
+(autoload 'web-mode "web-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(setq web-mode-engines-alist
+      '(("template-toolkit" . "\\.html?\\'" )))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+
+  ;; web-mode. colors.
+  (set-face-attribute 'web-mode-doctype-face nil :foreground "#4A8ACA")
+  (set-face-attribute 'web-mode-html-tag-face nil :foreground "#4A8ACA")
+  (set-face-attribute 'web-mode-html-attr-name-face nil :foreground "#87CEEB")
+  (set-face-attribute 'web-mode-html-attr-equal-face nil :foreground "#FFFFFF")
+  (set-face-attribute 'web-mode-html-attr-value-face nil :foreground "#D78181")
+  (set-face-attribute 'web-mode-comment-face nil :foreground "#587F35")
+  (set-face-attribute 'web-mode-server-comment-face nil :foreground "#587F35")
+
+  ;; web-mode. css colors.
+  (set-face-attribute 'web-mode-css-at-rule-face nil :foreground "#DFCF44")
+  (set-face-attribute 'web-mode-comment-face nil :foreground "#587F35")
+  (set-face-attribute 'web-mode-css-selector-face nil :foreground "#DFCF44")
+  (set-face-attribute 'web-mode-css-pseudo-class nil :foreground "#DFCF44")
+  (set-face-attribute 'web-mode-css-property-name-face nil :foreground "#87CEEB")
+  (set-face-attribute 'web-mode-css-string-face nil :foreground "#D78181")
+  )
+(add-hook 'web-mode-hook  'my-web-mode-hook)
