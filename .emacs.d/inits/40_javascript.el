8c84947e74d15cd1b13d48bdda61e6589c279d4b
Ichinose Shogo <shogo.ichinose@fuller.co.jp>
2016-03-15 19:49:33 +0900

    adjust js-mode settings

--- .emacs.d/inits/40_javascript.el
+++ .emacs.d/inits/40_javascript.el
@@ -2,6 +2,13 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
-; 外部で定義されているはずの変数・関数の警告抑制
+(add-hook 'js2-mode-hook
+          '(lambda ()
+             (setq-default indent-tabs-mode nil
+			   js-indent-level 4
+			   ) ; インデントにタブを使用しない
+	     ))
+
+;; 外部で定義されているはずの変数・関数の警告抑制
 (setq js2-global-externs
-      (list "require" "describe" "it" "expect"))
+      (list "require" "describe" "it" "expect" "$" "location" "process" "module"))
