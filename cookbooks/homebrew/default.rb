cf84138e23ba7fce6ad0997c80521f9870071f02
Ichinose Shogo <shogo82148@gmail.com>
2019-04-28 22:11:25 +0900

    homebrewをインストール

---
+++ cookbooks/homebrew/default.rb
@@ -0,0 +1,5 @@
+execute 'Install Homebrew' do
+  user node[:user]
+  command 'yes "" | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+  not_if 'test $(which /usr/local/bin/brew)'
+end
