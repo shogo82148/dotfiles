b55496c655e53433c6638a8d9b1b4f4d7d15e784
Ichinose Shogo <shogo82148@gmail.com>
2019-04-29 13:03:34 +0900

    bashの設定追加

--- cookbooks/bash/files/bash_profile.sh
+++ cookbooks/bash/files/bash_profile.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
 # .bash_profile
 
 if [ -x /usr/libexec/path_helper ]; then
-    PATH=""
     eval `/usr/libexec/path_helper -s`
 fi
 
@@ -15,26 +15,8 @@ if [ -f ~/.bashrc ]; then
     . ~/.bashrc
 fi
 
-export PATH="$HOME/.plenv/bin:$PATH"
-eval "$(plenv init -)"
-
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-
-export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-
-if [[ -s $HOME/.nvm/nvm.sh ]] ; then
-    source $HOME/.nvm/nvm.sh ;
-fi
-
 export GOPATH=$HOME
 export PATH="$GOPATH/bin:$PATH"
-
-# rust
-if [ -f ~/.cargo/env ]; then
-    . ~/.cargo/env
-fi
 
 if command -v aws_completer ; then
     complete -C "$(command -v aws_completer)" aws
@@ -44,7 +26,3 @@ fi
 if [ -f ~/.bashlocal ]; then
     . ~/.bashlocal
 fi
-
-test -e "${HOME}/.iterm2_shell_integration.bash" && source "${HOME}/.iterm2_shell_integration.bash"
-
-export PATH="$HOME/.cargo/bin:$PATH"
