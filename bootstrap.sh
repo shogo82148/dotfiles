51308c5238cd02b52a2959c2ff45675ff621ab65
Ichinose Shogo <shogo82148@gmail.com>
2020-02-19 15:42:21 +0900

    update mitamae

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ TMPDIR=$(mktemp -d)
 trap 'rm -rfv "$TMPDIR"' EXIT
 
 # install mitamae
-VERSION=1.7.5
+VERSION=1.11.0
 cd "$TMPDIR"
 curl -sSL "https://github.com/itamae-kitchen/mitamae/releases/download/v$VERSION/mitamae-x86_64-darwin.tar.gz" -o mitamae.tar.gz
 tar xzvf mitamae.tar.gz
