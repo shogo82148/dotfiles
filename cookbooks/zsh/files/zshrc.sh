5eb15847c215886bc79cbadc782b532cb7aba959
Ichinose Shogo <shogo82148@gmail.com>
2020-06-16 15:33:33 +0900

    pecoによる補間関数

--- cookbooks/zsh/files/zshrc.sh
+++ cookbooks/zsh/files/zshrc.sh
@@ -15,5 +15,30 @@ function peco-history-selection() {
 zle -N peco-history-selection
 bindkey '^R' peco-history-selection
 
+function pcd () {
+    local selected_dir=$(ghq list -p | peco)
+    if [ -n "$selected_dir" ]; then
+    cd "$selected_dir"
+    fi
+}
+
+function paws {
+    local profile=$(perl -nle 'print $1 if /^[[](?:profile\s+)?([^]]+)/' ~/.aws/config | peco)
+    if [ -n "$profile" ]; then
+        AWS_PROFILE=$profile
+        export AWS_PROFILE
+        echo "AWS_PROFILE is now $AWS_PROFILE"
+    fi
+}
+
+function penv {
+    local appenv=$( (echo development; echo staging; echo production) | peco)
+    if [ -n "$appenv" ]; then
+        APP_ENV=$appenv
+        export APP_ENV
+        echo "APP_ENV is now $APP_ENV"
+    fi
+}
+
 autoload bashcompinit && bashcompinit
 complete -C '/usr/local/bin/aws_completer' aws
