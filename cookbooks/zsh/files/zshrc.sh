89c55c025d49563cc079640a6cf60025b0c3d25b
Ichinose Shogo <shogo82148@gmail.com>
2020-06-16 15:18:26 +0900

    pecoによる履歴検索

--- cookbooks/zsh/files/zshrc.sh
+++ cookbooks/zsh/files/zshrc.sh
@@ -5,5 +5,15 @@ if command -v direnv; then
     eval "$(direnv hook bash)"
 fi
 
+# from https://qiita.com/shepabashi/items/f2bc2be37a31df49bca5
+function peco-history-selection() {
+    BUFFER=`history -n 1 | tail -r  | awk '!a[$0]++' | peco`
+    CURSOR=$#BUFFER
+    zle reset-prompt
+}
+
+zle -N peco-history-selection
+bindkey '^R' peco-history-selection
+
 autoload bashcompinit && bashcompinit
 complete -C '/usr/local/bin/aws_completer' aws
