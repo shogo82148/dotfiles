3516181f1a9b960c2bfa9722b144c71ba2519a21
Ichinose Shogo <shogo82148@gmail.com>
2020-06-16 11:07:48 +0900

    plenv/rbenvのパス修正

--- cookbooks/plenv/default.rb
+++ cookbooks/plenv/default.rb
@@ -1,7 +1,7 @@
 git "#{ENV['HOME']}/.plenv" do
-  repository "git@github.com/tokuhirom/plenv.git"
+  repository "git@github.com:tokuhirom/plenv.git"
 end
 
 git "#{ENV['HOME']}/.plenv/plugins/perl-build" do
-  repository "git@github.com/tokuhirom/Perl-Build.git"
+  repository "git@github.com:tokuhirom/Perl-Build.git"
 end
--- cookbooks/rbenv/default.rb
+++ cookbooks/rbenv/default.rb
@@ -1,8 +1,8 @@
 git "#{ENV['HOME']}/.rbenv" do
-  repository "git@github.com/rbenv/rbenv.git"
+  repository "git@github.com:rbenv/rbenv.git"
 end
 
 directory "#{ENV['HOME']}/.rbenv/plugins"
 git "#{ENV['HOME']}/.rbenv/plugins/ruby-build" do
-  repository "git@github.com/rbenv/ruby-build.git"
+  repository "git@github.com:rbenv/ruby-build.git"
 end
