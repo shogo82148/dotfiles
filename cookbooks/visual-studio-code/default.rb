3905c1e0cb7e5c12d37e82be441418d2c847cc8d
Ichinose Shogo <shogo82148@gmail.com>
2021-08-23 18:44:14 +0900

    remove vscode settings
    vscode supports settings sync

--- cookbooks/visual-studio-code/default.rb
+++
@@ -1,7 +0,0 @@
-remote_file "#{ENV['HOME']}/Library/Application Support/Code/User/locale.json" do
-  source "files/config/locale.json"
-end
-
-remote_file "#{ENV['HOME']}/Library/Application Support/Code/User/settings.json" do
-  source "files/config/settings.json"
-end
