58bb6e61d4571137419bd40ba2f36683f95ced27
Ichinose Shogo <shogo82148@gmail.com>
2022-01-25 10:31:42 +0900

    intall binaries to ~/bin

--- lib/function.rb
+++ lib/function.rb
@@ -1,18 +1,18 @@
 define :darwin_app, source: nil do
   source = params[:source] || params[:name]
-  execute "brew install #{source} --appdir=\"/Applications\" --cask" do
+  execute "brew install --cask #{source} --appdir=\"/Applications\"" do
     not_if "brew list --cask | grep -q #{source}"
   end
 end
 
 define :github_binary, version: nil, repository: nil, archive: nil, binary_path: nil do
   cmd = params[:name]
-  bin_path = "/usr/local/bin/#{cmd}"
+  bin_path = "#{ENV['HOME']}/bin/#{cmd}"
   archive = params[:archive]
   url = "https://github.com/#{params[:repository]}/releases/download/#{params[:version]}/#{archive}"
 
   if archive.end_with?('.zip')
-    extract = "unzip -o"
+    extract = "unzip -jo"
   elsif archive.end_with?('.tar.gz')
     extract = "tar xvzf"
   else
@@ -26,7 +26,7 @@ define :github_binary, version: nil, repository: nil, archive: nil, binary_path:
     not_if "test -f #{bin_path}"
     cwd "/tmp"
   end
-  execute "mv /tmp/#{params[:binary_path] || cmd} #{bin_path} && chmod +x #{bin_path}" do
+  execute "install -m 0755 /tmp/#{params[:binary_path] || cmd} #{bin_path}" do
     not_if "test -f #{bin_path}"
   end
 end
