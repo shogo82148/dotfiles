99503dba7e0c702ee1a8e44f1c7e04ae4661fde6
Ichinose Shogo <shogo82148@gmail.com>
2022-01-25 10:09:59 +0900

    Intel and M1 support

--- cookbooks/homebrew/default.rb
+++ cookbooks/homebrew/default.rb
@@ -1,5 +1,12 @@
 execute 'Install Homebrew' do
   user node[:user]
   command 'yes "" | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"'
-  not_if 'test $(which /opt/homebrew/bin/brew)'
+  case node[:arch]
+  when 'x86_64'
+    not_if 'test $(which /usr/local/bin/brew)'
+  when 'arm64'
+    not_if 'test $(which /opt/homebrew/bin/brew)'
+  else
+    raise ArgumentError, "unknown arch: #{node[:arch]}"
+  end
 end
